3cd771fff0e355ae299882f6096bf14f37926d39
AyushiK16 <62651426+AyushiK16@users.noreply.github.com>
2021-08-21 23:31:28 +0530

    Update SwipeableFlatlist.js

--- components/SwipeableFlatlist.js
+++ components/SwipeableFlatlist.js
@@ -1,134 +1,135 @@
-import React from 'react';
-import {Text, View, StyleSheet, TouchableOpacity, Dimensions} from 'react-native'
-import {DrawerItems} from 'react-navigation-drawer'
-import firebase from 'firebase'
-import {SwipeListView} from 'react-native-swipe-list-view'
-import db from '../config'
-import {Icon, ListItem} from 'react-native-elements'
+import React, { Component } from "react";
+import {
+  Animated,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from "react-native";
+import { ListItem, Icon } from "react-native-elements";
 
-export default class SwipeableFlatlist extends React.Component{
-    constructor(props){
-        super(props);
-        console.log('swipable flatlist:', this.props.allNotifications)
-        this.state = ({
-            allNotifications : this.props.allNotifications
-        })
+import { SwipeListView } from "react-native-swipe-list-view";
+
+import db from "../config";
+
+export default class SwipeableFlatlist extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allNotifications: this.props.allNotifications,
+      flag: 0,
+      refresh: false
+    };
+  }
+
+  
+  componentWillReceiveProps(nextProps) {
+    this.setState({ allNotifications: nextProps.allNotifications });  
+  }
+
+
+
+  updateMarkAsread = notification => {
+    db.collection("ReminderAdds")
+      .doc(notification.doc_id)
+      .update({
+        notificationStatus: 1
+      });
+  };
+
+  onSwipeValueChange = swipeData => {
+    const { key, value } = swipeData;
+    if(value<0 && value>=-10){
+      this.setState({
+        flag:0
+      })
     }
-
-    onSwipeValueChange = (swipeData) => {
-      var allNotifications = this.state.allNotifications
-      const {key, value} = swipeData
-      console.log("KEY: ", key)
-      if(value < -Dimensions.get('window').width){
-        //made a copy of the array
-        var newData = [...allNotifications];
-        //passing the notification which has been swiped.
-        this.updateStatus(allNotifications[key]);
-        //.splice is used to add or remove something from an array.
-        //here it will remove this current index, and 1 represents the value to delete.
-        //will remove it from the array
-        newData.splice(key,1);
+    if(this.state.flag === 0) {
+      var allNotifications = this.state.allNotifications;
+      if (value < -Dimensions.get("window").width) {       
+        const newData = [...allNotifications];
+        this.updateMarkAsread(allNotifications[key]);
+        newData.splice(key, 1);
         this.setState({
-          allNotifications : newData
-        })
-        
+          allNotifications: newData,
+          flag: 1,
+          refresh: !this.state.refresh });
       }
     }
-
-    updateStatus(notification){
-      db.collection('ReminderAdds').doc(notification.doc_id)
-      .update({notificationStatus: 1})
-      //1 MEANS THAT THE NOTIF IS READ
-    }
-
-    keyExtractor = (item, index) => index.toString()
-
     
-    renderItem = ({item,i}) => {
-    return (
-      <ListItem key={i} bottomDivider>
+  };
+
+  renderItem = data => (
+    <Animated.View>
+      <ListItem bottomDivider>
         <ListItem.Content>
-          <ListItem.Title>{item.reminderDate}</ListItem.Title>
-          <ListItem.Subtitle>{item.details}</ListItem.Subtitle>
+          <ListItem.Title>{data.item.reminderDate}</ListItem.Title>
+          <ListItem.Subtitle>{data.item.details}</ListItem.Subtitle>
         </ListItem.Content>
       </ListItem>
-      
-    )
-    /*
-    added after item.reminder data line
-              {<Icon name = "book" type = "font-awesome" color = '#696969'/>}  
+    </Animated.View>
+  );
 
-    */
-  }
-
-  renderHiddenItem(){
-    return(
-    <View style = {styles.rowBack}>
-      <View style = {[styles.backRightBtn, styles.backRightBtnRight]}>
-        <Text style = {styles.backTextWhite}>
-          Task Done
-        </Text>
-
+  renderHiddenItem = () => (
+    <View style={styles.rowBack}>
+      <View style={[styles.backRightBtn, styles.backRightBtnRight]}>
+        <Text style={styles.backTextWhite}>Mark as read</Text>
       </View>
+    </View>
+  );
 
-    </View>)
-  }
-
-  componentWillReceiveProps(nextProps){
-    this.setState({
-      allNotifications: nextProps.allNotifications
-    })
-  }
-
-  render(){
-    return(
-      <View style = {styles.container}>
+  render() {
+    return (
+      <View style={styles.container}>
         <SwipeListView
-        disableRightSwipe
-        data = {this.state.allNotifications}
-        renderItem = {this.renderItem}
-        renderHiddenItem = {this.renderHiddenItem}
-        rightOpenValue = {-Dimensions.get('window').width}
-        onSwipeValueChange = {this.onSwipeValueChange}
-        keyExtractor = {this.keyExtractor}
+          disableRightSwipe
+          data={this.state.allNotifications}
+          renderItem={this.renderItem}
+          renderHiddenItem={this.renderHiddenItem}
+          rightOpenValue={-Dimensions.get("window").width}
+          previewRowKey={"0"}
+          previewOpenValue={-40}
+          previewOpenDelay={3000}
+          onSwipeValueChange={this.onSwipeValueChange}
+          keyExtractor={(item, index) => index.toString()}
+          key={this.state.refresh}
         />
       </View>
-    )
+    );
   }
 }
 
-
-
 const styles = StyleSheet.create({
-    container: {
-      backgroundColor: "white",
-      flex: 1
-    },
-    backTextWhite: {
-      color: "#FFF",
-      fontWeight: "bold",
-      fontSize: 15,
-      textAlign: "center",
-      alignSelf: "flex-start"
-    },
-    rowBack: {
-      alignItems: "center",
-      backgroundColor: "#29b6f6",
-      flex: 1,
-      flexDirection: "row",
-      justifyContent: "space-between",
-      paddingLeft: 15
-    },
-    backRightBtn: {
-      alignItems: "center",
-      bottom: 0,
-      justifyContent: "center",
-      position: "absolute",
-      top: 0,
-      width: 100
-    },
-    backRightBtnRight: {
-      backgroundColor: "#29b6f6",
-      right: 0
-    }
-  });
+  container: {
+    backgroundColor: "white",
+    flex: 1
+  },
+  backTextWhite: {
+    color: "#FFF",
+    fontWeight: "bold",
+    fontSize: 15,
+    textAlign: "center",
+    alignSelf: "flex-start"
+  },
+  rowBack: {
+    alignItems: "center",
+    backgroundColor: "#29b6f6",
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingLeft: 15
+  },
+  backRightBtn: {
+    alignItems: "center",
+    bottom: 0,
+    justifyContent: "center",
+    position: "absolute",
+    top: 0,
+    width: 100
+  },
+  backRightBtnRight: {
+    backgroundColor: "#29b6f6",
+    right: 0
+  }
+});
